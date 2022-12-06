b7a6879cf405ec6fa41316503183f5e4bdd292d4
Mohd Danish <63279839+mohddanish-cse@users.noreply.github.com>
2022-12-06 13:13:51 +0000

    added example outputs

--- pattern7.cpp
+++ pattern7.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int count =1;
+    int count =n;
     for(int i=1;i<=n;i++)
     {
-       for(int j=1;j<=i;j++) {
-            cout<<count++<<" ";
-        }
+       for(int j=i;j>0;j--) {
+            cout<<j<<" ";
+           }
         cout<<endl;
     }
     return 0;
@@ -16,7 +16,7 @@ int main() {
 
 //Output
 // 1 
-// 2 3 
-// 4 5 6 
-// 7 8 9 10 
-// 11 12 13 14 15 
+// 2 1 
+// 3 2 1 
+// 4 3 2 1 
+// 5 4 3 2 1 
