fbb8b6f109673e9f4e7de9283a19dd2f1769bfe5
Mohd Danish <63279839+mohddanish-cse@users.noreply.github.com>
2022-12-07 16:17:18 +0000

    initial commit

---
+++ star_patterns/star_pattern4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  int j=n;
+  for(int i=1;i<=n;i++) {
+      
+    // for (int j=n;j>=i;j--) {
+    //   cout<<'*';
+    // }
+    string star(j--,'*');
+    cout<<star;
+
+    cout<<endl;
+  }
+  return 0;
+}
+
+// *****
+// ****
+// ***
+// **
+// *
---
+++ star_patterns/star_pattern5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int n;
+ cin>>n;
+ int j=n;
+ for(int i=0;i<n;i++) {
+  string space(i, ' ');
+  cout << space;
+  string star(j--,'x');
+  cout << star<<endl;
+ }
+ return 0;
+}
+
+// xxxxx
+//  xxxx
+//   xxx
+//    xx
+//     x
